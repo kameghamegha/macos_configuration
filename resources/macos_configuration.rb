76e3a8c3ab9e8a149b63ea16c241a0730b0db916
Meg Cassidy <kameghamegha@users.noreply.github.com>
2016-10-27 16:30:02 -0700

    Fixed user property and renamed properties from attributes

--- resources/macos_configuration.rb
+++ resources/macos_configuration.rb
@@ -3,29 +3,29 @@ resource_name :macos_configuration
 actions :enable, :disable, :set
 default_action :set
 
-
 # TODO: Make a list of the available features
-attribute :feature,
+property :feature,
           kind_of: String,
           name_attribute: true,
           required: true
 
 # A topic is a named group of features
-attribute :topic,
+property :topic,
           kind_of: String,
           required: true
 
 # Only use this when using the :set action, as this will be used for any
 # settings that are not boolean.
 # TODO: Return error if value is set for actions :enable and :disable
-attribute :value,
+property :value,
           kind_of: [String, Integer],
           required: false
 
 # Run as a specific user, otherwise run as node's current user
-attribute :user,
+property :user,
           kind_of: String,
-          default: node['harden_os']['user'],
+          #TODO(meg): Import this from my hardening recipe
+          default: node['macos_config']['user'],
           required: true
 
 action :enable do
